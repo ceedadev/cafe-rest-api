98582bf8e67bf8c7ff3df4123917703904501c53
Armanda Arif <ceedadev@gmail.com>
2021-02-18 19:39:35 +0700

    added HelloAPIView

--- cafe_api/views.py
+++ cafe_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import response
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloAPIView(APIView):
+    """Test API View"""
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP method as function (get, post, put, delete)',
+            'is similar to traditional Django view',
+            'Gives you the most of application logic',
+            'Is mapped manually to URLs'
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
+        
